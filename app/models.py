94846735bbeeeedc2e045f8f2e0f921f56ea47b9
Connor Keevill <connor.keevill.19@gmail.com>
2021-02-17 21:54:37 +0000

    Delete undeeded model :')

--- app/models.py
+++ app/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 
-class User(models.Model):
-	first_name = models.CharField(max_length=30)
-	surname = models.CharField(max_length=30)
-	date_of_birth = models.DateField()
