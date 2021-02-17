8500dd6322f1429e6960480a33bb8c5eb9f6878b
Connor Keevill <connor.keevill.19@gmail.com>
2021-02-17 21:27:04 +0000

    Create User model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+	first_name = models.CharField(max_length=30)
+	surname = models.CharField(max_length=30)
+	date_of_birth = models.DateField()
