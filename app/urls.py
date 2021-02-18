3d62be90db674f4d5ac2693214ac23f810f0fea8
Connor Keevill <connor.keevill.19@gmail.com>
2021-02-18 16:41:25 +0000

    A few things: Create a class to handle the HTTP requests for the signup page Alter the urlpattern to reflect this Change the signup.html to use the form that is generated by signup

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path('forgot-password/', views.forgot_password, name='forgot-password'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,18 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
 
 
 def login(request):
-    return render(request, 'app/login.html')
+	return render(request, 'app/login.html')
 
 
 def dashboard(request):
-    return render(request, 'app/index.html')
+	return render(request, 'app/index.html')
 
 
-def signup(request):
-    return render(request, 'app/signup.html')
+class SignUp(View):
+
+	def get(self, request, form=None):
+		if form is None:
+			form = UserCreationForm()
+		return render(request, 'app/signup.html', {'form': form})
+
+	def post(self, request):
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			return HttpResponse("I'm happy")
+		else:
+			return self.get(request, form)
 
 
 def forgot_password(request):
-    return render(request, 'app/password.html')
+	return render(request, 'app/password.html')
