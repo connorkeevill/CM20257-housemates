c9ad76785d33a944c7029eabb3ed9f46405163b9
Connor Keevill <connor.keevill.19@gmail.com>
2021-02-27 14:49:04 +0000

    indent with tabs

--- app/views.py
+++ app/views.py
@@ -7,32 +7,32 @@ from app.models import Task
 
 
 def dashboard(request):
-    calendar = create_calendar()
-    tasks = {'tasks': Task.objects.all(),
-             'calendar': calendar}
-    # payments = {'payments': Payment.object.all()}
-    return render(request, 'app/index.html', tasks)
+	calendar = create_calendar()
+	tasks = {'tasks': Task.objects.all(),
+			 'calendar': calendar}
+	# payments = {'payments': Payment.object.all()}
+	return render(request, 'app/index.html', tasks)
 
 
 class SignUp(View):
 
-    def get(self, request, form=None):
-        # If we haven't been passed a priorly created form, create a new one
-        if form is None:
-            form = UserCreationForm()
+	def get(self, request, form=None):
+		# If we haven't been passed a priorly created form, create a new one
+		if form is None:
+			form = UserCreationForm()
 
-        return render(request, 'app/signup.html', {'form': form})
+		return render(request, 'app/signup.html', {'form': form})
 
-    def post(self, request):
-        form = UserCreationForm(request.POST)
+	def post(self, request):
+		form = UserCreationForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            return redirect("login")
-        else:
-            # We pass the form to the get request to allow it to populate it with the data that was there before then
-            return self.get(request, form)
+		if form.is_valid():
+			form.save()
+			return redirect("login")
+		else:
+			# We pass the form to the get request to allow it to populate it with the data that was there before then
+			return self.get(request, form)
 
 
 def forgot_password(request):
-    return render(request, 'app/password.html')
+	return render(request, 'app/password.html')
