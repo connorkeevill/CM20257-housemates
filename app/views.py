ae2f64e37a6871dea058bae1efcb981761c885c8
Connor Keevill <connor.keevill.19@gmail.com>
2021-02-28 22:11:53 +0000

    Alter signup system to require firstname and surname from user when creating new account

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
+from .forms import *
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -16,22 +17,31 @@ def dashboard(request):
 
 class SignUp(View):
 
-	def get(self, request, form=None):
-		# If we haven't been passed a priorly created form, create a new one
-		if form is None:
-			form = UserCreationForm()
+	def get(self, request, userForm=None, profileForm=None):
+		# If we haven't been passed a priorly created user form, create a new one
+		if userForm is None:
+			userForm = UserRegistrationForm()
 
-		return render(request, 'app/signup.html', {'form': form})
+		# Likewise for the profile form
+		if profileForm is None:
+			profileForm = ProfileRegistrationForm()
+
+		return render(request, 'app/signup.html', {'userForm': userForm,
+												   'profileForm': profileForm})
 
 	def post(self, request):
-		form = UserCreationForm(request.POST)
+		userForm = UserRegistrationForm(request.POST)
+		profileForm = ProfileRegistrationForm(request.POST)
 
-		if form.is_valid():
-			form.save()
+		if userForm.is_valid() and profileForm.is_valid():
+			userForm.instance.Profile = profileForm.instance
+			profileForm.instance.User = userForm.instance
+			userForm.save()
+			profileForm.save()
 			return redirect("login")
 		else:
-			# We pass the form to the get request to allow it to populate it with the data that was there before then
-			return self.get(request, form)
+			# We pass the userForm to the get request to allow it to populate it with the data that was there before then
+			return self.get(request, userForm, profileForm)
 
 
 def forgot_password(request):
