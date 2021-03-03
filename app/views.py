558aa3a6f3b75fbf2913f01837512dfcf772c1fe
Connor Keevill <connor.keevill.19@gmail.com>
2021-03-03 10:57:14 +0000

    tabs

--- app/views.py
+++ app/views.py
@@ -1,62 +1,61 @@
-from django.contrib.auth.forms import UserCreationForm
-from .forms import *
 from django.shortcuts import render, redirect
 from django.views import View
 
 from app.logic.calendar import create_calendar
 from app.models import Task
+from .forms import *
 
 
 def dashboard(request):
-    if request.method == 'GET':
-        calendar = create_calendar()
+	if request.method == 'GET':
+		calendar = create_calendar()
 
-        # payments = {'payments': Payment.object.all()}
-        return render(request, 'app/index.html', {'tasks': Task.objects.all(), 'calendar': calendar})
-    else:
-        calendar = create_calendar()
-        tasks = {'tasks': Task.objects.all(),
-                 'calendar': calendar}
+		# payments = {'payments': Payment.object.all()}
+		return render(request, 'app/index.html', {'tasks': Task.objects.all(), 'calendar': calendar})
+	else:
+		calendar = create_calendar()
+		tasks = {'tasks': Task.objects.all(),
+				 'calendar': calendar}
 
-        task_name = request.POST.get('task_name')
-        task_description = request.POST.get('task_description')
-        task_date_due = request.POST.get('task_date_due')
+		task_name = request.POST.get('task_name')
+		task_description = request.POST.get('task_description')
+		task_date_due = request.POST.get('task_date_due')
 
-        # need to get author from
-        new_task = Task(title=task_name, description=task_description, date_due=task_date_due, author=request.user)
-        new_task.save()
+		# need to get author from
+		new_task = Task(title=task_name, description=task_description, date_due=task_date_due, author=request.user)
+		new_task.save()
 
-        return render(request, 'app/index.html', tasks)
+		return render(request, 'app/index.html', tasks)
 
 
 class SignUp(View):
 
-    def get(self, request, userForm=None, profileForm=None):
-        # If we haven't been passed a priorly created user form, create a new one
-        if userForm is None:
-            userForm = UserRegistrationForm()
+	def get(self, request, userForm=None, profileForm=None):
+		# If we haven't been passed a priorly created user form, create a new one
+		if userForm is None:
+			userForm = UserRegistrationForm()
 
-        # Likewise for the profile form
-        if profileForm is None:
-            profileForm = ProfileRegistrationForm()
+		# Likewise for the profile form
+		if profileForm is None:
+			profileForm = ProfileRegistrationForm()
 
-        return render(request, 'app/signup.html', {'userForm': userForm,
-                                                   'profileForm': profileForm})
+		return render(request, 'app/signup.html', {'userForm': userForm,
+												   'profileForm': profileForm})
 
-    def post(self, request):
-        userForm = UserRegistrationForm(request.POST)
-        profileForm = ProfileRegistrationForm(request.POST)
+	def post(self, request):
+		userForm = UserRegistrationForm(request.POST)
+		profileForm = ProfileRegistrationForm(request.POST)
 
-        if userForm.is_valid() and profileForm.is_valid():
-            userForm.instance.Profile = profileForm.instance
-            profileForm.instance.User = userForm.instance
-            userForm.save()
-            profileForm.save()
-            return redirect("login")
-        else:
-            # We pass the userForm to the get request to allow it to populate it with the data that was there before then
-            return self.get(request, userForm, profileForm)
+		if userForm.is_valid() and profileForm.is_valid():
+			userForm.instance.Profile = profileForm.instance
+			profileForm.instance.User = userForm.instance
+			userForm.save()
+			profileForm.save()
+			return redirect("login")
+		else:
+			# We pass the userForm to the get request to allow it to populate it with the data that was there before then
+			return self.get(request, userForm, profileForm)
 
 
 def forgot_password(request):
-    return render(request, 'app/password.html')
+	return render(request, 'app/password.html')
