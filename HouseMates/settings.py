3993d92ce1bb3fd37661aa4c19d89fd0bb20e590
Connor Keevill <connor.keevill.19@gmail.com>
2021-03-07 10:57:22 +0000

    Turn on debug mode

--- HouseMates/settings.py
+++ HouseMates/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z$23d7ifrjg0+l70*jdphem+t!&+pniuzgjmqf1^+(81ous11*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
