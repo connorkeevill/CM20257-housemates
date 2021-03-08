3394728decaebdc2c6c7184380f4d6e704b2668d
Connor Keevill <connor.keevill.19@gmail.com>
2021-03-08 00:07:47 +0000

    Remove redundant brackets

--- app/models.py
+++ app/models.py
@@ -58,7 +58,7 @@ class Task(models.Model):
 	description = models.TextField()
 	date_due = models.DateTimeField()
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
-	hosue = models.ForeignKey(House, on_delete=models.CASCADE())
+	hosue = models.ForeignKey(House, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
